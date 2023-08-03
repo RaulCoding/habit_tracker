1ae87b06efbaf32b3550fa937264f0bc77ca34c8
Raúl Viloria <raulcodelearning@gmail.com>
2023-08-03 18:58:54 +0200

    App en funcionamiento

--- lib/data/habit_databse.dart
+++ lib/data/habit_databse.dart
@@ -5,40 +5,98 @@ import 'package:hive_flutter/hive_flutter.dart';
 final _myBox = Hive.box("Habit_Database");
 
 class HabitDatabase {
-
   List todaysHabitList = [];
-  
+  Map<DateTime, int> heatMapDataSet = {};
+
   // Crear datos predeterminados iniciales
-  void createDefaultData (){
+  void createDefaultData() {
     todaysHabitList = [
       ["Correr", false],
       ["Leer", false],
     ];
-    
+
     _myBox.put("START_DATE", todaysDateFormatted());
   }
+
   //Cargar datos si es que existen
-  void loadData(){
+  void loadData() {
     // Si es un nuevo dia, toma la lista habitos de la db
-    if(_myBox.get(todaysDateFormatted()) == null) {
+    if (_myBox.get(todaysDateFormatted()) == null) {
       todaysHabitList = _myBox.get("CURRENT_HABIT_LIST");
       // Marcar todos los datos como falsos ya que es un nuevo día
-      for (int i = 0; i < todaysHabitList.length; i++){
-      todaysHabitList[i][1] = false;
+      for (int i = 0; i < todaysHabitList.length; i++) {
+        todaysHabitList[i][1] = false;
       }
     }
     //Si no es un nuevo día, cargar la lista de hoy
-    else{
+    else {
       todaysHabitList = _myBox.get(todaysDateFormatted());
     }
   }
-  
-  
+
   //Actualizar base de datos
-  void updateDatabase(){
+  void updateDatabase() {
     // Actualizar la entrada de hoy
     _myBox.put(todaysDateFormatted(), todaysHabitList);
     //Actualizar la lista universal de Habitos en caso de que esta haya cambiado (nuevo ahabito, editar habito, borrar habito)
     _myBox.put("CURRENT_HABIT_LIST", todaysHabitList);
+
+    //Calcular porcentajes diarios de habitos completos
+    calculateHabitPercentages();
+
+    //Cargar Heat Map
+    loadHeatMap();
+  }
+
+  void calculateHabitPercentages() {
+    int countCompleted = 0;
+    for (int i = 0; i < todaysHabitList.length; i++) {
+      if (todaysHabitList[i][1] == true) {
+        countCompleted++;
+      }
+    }
+    String percent = todaysHabitList.isEmpty
+        ? "0.0"
+        : (countCompleted / todaysHabitList.length).toStringAsFixed(1);
+
+    //key: "PERCENTAGE_SUMMARY_YYYYMMDD"
+    //value: string de 1 numero decimal entre 0.0 y 1.0 inclusives
+    _myBox.put("PERCENTAGE_SUMMARY_${todaysDateFormatted()}", percent);
+  }
+
+  void loadHeatMap() {
+    DateTime startDate = createDateTimeObject(_myBox.get("START_DATE"));
+
+    // Cuenta el numero  de días a cargar
+    int daysInBetween = DateTime.now().difference(startDate).inDays;
+
+    // go from start date to today and add each percentage to the dataset
+    // "PERCENTAGE_SUMMARY_yyyymmdd" will be the key in the database
+    for (int i = 0; i < daysInBetween + 1; i++) {
+      String yyyymmdd = convertDateTimeToString(
+        startDate.add(Duration(days: i)),
+      );
+
+      double strengthAsPercent = double.parse(
+        _myBox.get("PERCENTAGE_SUMMARY_$yyyymmdd") ?? "0.0",
+      );
+
+      //Separa el DateTime como abajo para que asi no se tenga que preocupar por horas/minutos/segundos
+      // Año
+      int year = startDate.add(Duration(days: i)).year;
+
+      // Mes
+      int month = startDate.add(Duration(days: i)).month;
+
+      // Día
+      int day = startDate.add(Duration(days: i)).day;
+
+      final percentForEachDay = <DateTime, int>{
+        DateTime(year, month, day): (10 * strengthAsPercent).toInt(),
+      };
+
+      heatMapDataSet.addEntries(percentForEachDay.entries);
+      print(heatMapDataSet);
+    }
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/components/month_summary.dart';
 import 'package:habit_tracker/components/my_fab.dart';
 import 'package:habit_tracker/data/habit_databse.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -14,43 +15,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   HabitDatabase db = HabitDatabase();
-  final _myBox = Hive.box("Habit_Database");  
-  
-  
+  final _myBox = Hive.box("Habit_Database");
+
   @override
   void initState() {
-    
     //Si no hay una lista de habitos aun, es la 1ª vez que se abre la app.
     //Entonces crear datos predeterminados
-    
-    if(_myBox.get("CURRENT_HABIT_LIST") == null){
+
+    if (_myBox.get("CURRENT_HABIT_LIST") == null) {
       db.createDefaultData();
     }
     // Si ya existen datos, no es la 1ª vez
-    else{
+    else {
       db.loadData();
     }
     // Actualizar la base de datos
     db.updateDatabase();
-    
+
     super.initState();
   }
-  
+
   //checkbox pulsada
-  void checkBoxTapped(bool? value, int index){
+  void checkBoxTapped(bool? value, int index) {
     setState(() {
       db.todaysHabitList[index][1] = value;
     });
     db.updateDatabase();
   }
-  
+
   //Crear un nuevo habito
   final _newHabitNameController = TextEditingController();
-  void createaNewHabit(){
+  void createaNewHabit() {
     //Mostrar un AlertBox al usuario para introducir un nuevo habito
     showDialog(
-      context: context, 
-      builder: (context){
+      context: context,
+      builder: (context) {
         return MyAlertBox(
           controller: _newHabitNameController,
           hintText: "Nombre del hábito",
@@ -60,48 +59,46 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
-  
+
   // Guardar nuevo hábito
-  void saveNewHabit(){
+  void saveNewHabit() {
     //Añadir un hábito a la lista de hoy
     setState(() {
-    db.todaysHabitList.add([_newHabitNameController.text, false]);
+      db.todaysHabitList.add([_newHabitNameController.text, false]);
     });
     //Limpiar el cuadro de texto
     _newHabitNameController.clear();
 
     //Salir de la ventana
     Navigator.of(context).pop();
-    
+
     db.updateDatabase();
-    
   }
-    
-    
+
   //Cancelar el nuevo habito
-  void cancelDialogBox(){
+  void cancelDialogBox() {
     //Limpiar cuadro de texto
     _newHabitNameController.clear();
     //Salir de la ventana
     Navigator.of(context).pop();
   }
+
   // Abrir ventana editar habito
-  void openHabitSettings(int index){
+  void openHabitSettings(int index) {
     showDialog(
-      context: context , 
-      builder: (context){
-        return MyAlertBox(
-          controller: _newHabitNameController, 
-          hintText: db.todaysHabitList[index][0] ,
-          onSave: () => SaveExistingHabit(index), 
-          onCancel: cancelDialogBox,
-        );
-      }
-    );
+        context: context,
+        builder: (context) {
+          return MyAlertBox(
+            controller: _newHabitNameController,
+            hintText: db.todaysHabitList[index][0],
+            onSave: () => saveExistingHabit(index),
+            onCancel: cancelDialogBox,
+          );
+        });
   }
-  
+
   //Guardar habito existente con un nuevo nombre
-  void SaveExistingHabit(int index){
+  void saveExistingHabit(int index) {
     setState(() {
       db.todaysHabitList[index][0] = _newHabitNameController.text;
     });
@@ -109,35 +106,45 @@ class _HomePageState extends State<HomePage> {
     Navigator.pop(context);
     db.updateDatabase();
   }
-  
+
   // delete habit
-  void deleteHabit(int index){
+  void deleteHabit(int index) {
     setState(() {
       db.todaysHabitList.removeAt(index);
     });
     db.updateDatabase();
   }
-    
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      floatingActionButton: MyFloatingActionButton(
-        onPressed: createaNewHabit,
-      ),
-      body: ListView.builder(
-        itemCount: db.todaysHabitList.length,
-        itemBuilder: (context, index){
-          return HabitTile(
-            habitName: db.todaysHabitList[index][0], 
-            habitCompleted: db.todaysHabitList[index][1],
-            onChanged: (value) => checkBoxTapped(value, index),
-            settingsTapped:(context) => openHabitSettings(index),
-            deleteTapped: (context) => deleteHabit(index),
-          );
-        }
-        )
-    );
+        backgroundColor: Colors.grey[300],
+        floatingActionButton: MyFloatingActionButton(
+          onPressed: createaNewHabit,
+        ),
+        body: ListView(
+          children: [
+            //Resumen mensual del Heat Map
+            MonthlySummary(
+              datasets: db.heatMapDataSet, 
+              startDate: _myBox.get("START_DATE")
+            ),
+
+            //Lista de Habitos
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: db.todaysHabitList.length,
+                itemBuilder: (context, index) {
+                  return HabitTile(
+                    habitName: db.todaysHabitList[index][0],
+                    habitCompleted: db.todaysHabitList[index][1],
+                    onChanged: (value) => checkBoxTapped(value, index),
+                    settingsTapped: (context) => openHabitSettings(index),
+                    deleteTapped: (context) => deleteHabit(index),
+                  );
+                })
+          ],
+        ));
   }
 }
